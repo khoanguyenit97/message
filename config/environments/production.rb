4ba74f6439725874a64be76fe68f12457c7e7a97
khoanda10 <khoanda10@gmail.com>
2022-03-14 23:30:29 +0700

    k

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.mount_path = nil
+  config.action_cable.url = 'wss://enigmatic-taiga-98420.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = ['https://enigmatic-taiga-98420.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
