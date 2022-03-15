4e64034d9d7bcd6278e7cbe7792e0ebc7f74af74
khoanda10 <khoanda10@gmail.com>
2022-03-15 09:38:34 +0700

    fix text input

--- app/javascript/packs/chatroom/index.js
+++ app/javascript/packs/chatroom/index.js
@@ -5,12 +5,15 @@ scroll_bottom = function (){
 }
 
 submit_message = function (){
-    $('message_body').on('keydown', function (e){
+    $('#message_body').on('keydown', function (e){
         if (e.keyCode == 13){
             $('button').click();
             e.target.value = "";
+            $('message_body').value = "";
+
         };
     });
+
 };
 
 $(document).on('turbolinks:load', function(){
