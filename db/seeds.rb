21c2595f92d5b25dfd2b1d94a8eae84190a9317d
khoanda10 <khoanda10@gmail.com>
2022-03-22 22:38:40 +0700

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(username: 'KhoaNguyen', password: 'password')
 User.create(username: 'TuanPham', password: 'TuanPham')
-User.create(username: 'Khoa2', password: 'password')
+User.create(username: 'ThaoNgan', password: '00000000')
 User.create(username: 'Khoa3', password: 'password')
 User.create(username: 'Khoa4', password: 'password')
